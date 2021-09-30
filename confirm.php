d180e52344dfc497e8b2c410419e8aa92cdc4f46
AGuchkov <87633877+AGuchkov@users.noreply.github.com>
2021-09-30 16:52:23 +0300

    Add files via upload

--- confirm.php
+++ confirm.php
@@ -6,6 +6,14 @@ $quantity = $_POST['quantity'];
 $guestname2 = $_POST['guestname2'];
 $children = $_POST['children'];
 $drinks = $_POST['drinks'];
+
+//$quantity = $_POST['quantity'];
+//$guestname2 = $_POST['guestname2'];
+//$children = $_POST['children'];
+//$drinks = $_POST['drinks'];
+
+
+
 $token = "1893345900:AAFQyCb-z7bjZTLJMqGx4UMTaDOWe-JWxwc";
 $chat_id = "-473872660";
 $arr = array(
@@ -16,6 +24,22 @@ $arr = array(
   'С детьми:' => $children,
   'Предпочитает:' => $drinks
 );
+
+if($_POST['confirmation'] == 'Нет'){ 
+  $arr = array_diff($arr, array(
+      'Колличество:' => $quantity,
+      'Имя спутника:' => $guestname2,
+      'С детьми:' => $children,
+      'Предпочитает:' => $drinks
+      ));
+} elseif($_POST['quantity'] == 'Один'){ 
+  $arr = array_diff($arr, array(
+      'Имя спутника:' => $guestname2
+      ));
+} elseif($_POST['children'] == ''){ 
+  $arr = array_diff($arr, array(
+      'С детьми:' => $children
+      ));}
 
 foreach($arr as $key => $value) {
   $txt .= "<b>".$key."</b> ".$value."%0A";
